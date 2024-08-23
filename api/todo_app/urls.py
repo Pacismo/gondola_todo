d894766aaca72fbb08123901fa6828dabe5f4bee
Francisco Santana <francisco.santana2002@gmail.com>
2024-08-22 19:14:13 -0500

    Ordering

--- api/todo_app/urls.py
+++ api/todo_app/urls.py
@@ -4,6 +4,6 @@ from . import views;
 urlpatterns = [
     path('', views.get_tasks, {}),
     path('add', views.add_task, {}),
-    path('remove', views.remove_task, {}),
     path('mark', views.mark_task, {}),
+    path('remove', views.remove_task, {}),
 ]
